72164846b527d4c71f970ba1151544d0a30fa6b5
Kevin McKelvin <kmckelvin@gmail.com>
2010-06-24 15:14:23 +0200

    Updated vsh function in PowerShell Profile to just invoke the sln

--- Microsoft.PowerShell_Profile.ps1
+++ Microsoft.PowerShell_Profile.ps1
@@ -1,6 +1,8 @@
 # Add Git to the PS path
 $Env:Path = $Env:Path + ";C:\devtools\git\bin"
 
+cd "C:\@Code"
+
 # Always import psake
 Import-Module("C:\devtools\psake\psake.psm1")
 Set-Alias psake Invoke-psake
@@ -21,21 +23,22 @@ function vsh {
     param ($param)
    
     if ($param -eq $NULL) {
-        “A solution was not specified, opening the first one found.”
-        $solutions = get-childitem | ?{ $_.extension -eq “.sln” }
+        "A solution was not specified, opening the first one found."
+        $solutions = Get-ChildItem | ?{ $_.extension -eq “.sln” }
     }
     else {
-        “Opening {0} …” -f $param
-        vs $param
+        "Opening {0} ..." -f $param
+        Invoke-Item $param
         break
     }
+	
     if ($solutions.count -gt 1) {
-        “Opening {0} …” -f $solutions[0].Name
-        vs $solutions[0].Name
+        "Opening {0} ..." -f $solutions[0].Name
+        Invoke-Item $solutions[0].Name
     }
     else {
-        “Opening {0} …” -f $solutions.Name
-        vs $solutions.Name
+        "Opening {0} ..." -f $solutions.Name
+        Invoke-Item $solutions.Name
     }
 }
 
